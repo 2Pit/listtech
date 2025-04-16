39ababd83bd65304e8bbd725582dc525ba351b46
b_petr <afnm47@gmail.com>
2025-04-16 19:03:42 +0200

    init searcher

---
+++ searcher/build.rs
@@ -0,0 +1,21 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut config = prost_build::Config::new();
+
+    config.compile_well_known_types();
+    // .type_attribute(".", "#[derive(Debug)]");
+
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .out_dir("src/api/proto")
+        .compile_with_config(
+            config,
+            &["proto/api.proto"],
+            &["proto", "../corelib/proto/third_party"],
+        )?;
+
+    println!("cargo:rerun-if-changed=proto/api.proto");
+    println!("cargo:rerun-if-changed=../corelib/proto/third_party");
+
+    Ok(())
+}
--- searcher/src/main.rs
+++ searcher/src/main.rs
@@ -1,4 +1,23 @@
-fn main() {
-    println!("Starting searcher...");
-    // TODO: Add tracing setup and run logic
+mod index;
+
+use crate::index::SearchIndex;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().ok();
+    init_logging();
+
+    let index = SearchIndex::open_from_path("./data/index")?;
+    println!("Index and reader successfully initialized.");
+
+    Ok(())
+}
+
+fn init_logging() {
+    use tracing_subscriber::EnvFilter;
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::try_from_default_env().unwrap())
+        .with_file(true)
+        .with_line_number(true)
+        .init();
 }
